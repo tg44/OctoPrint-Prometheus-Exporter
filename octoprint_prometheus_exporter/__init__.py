d25fc567a8fade3bb61f81d36a2c723286733484
Gergo Torcsvari <torcsvari.gergo@gmail.com>
2019-12-08 14:55:07 +0100

    added metrics + fixed metric naming conventions

--- octoprint_prometheus_exporter/__init__.py
+++ octoprint_prometheus_exporter/__init__.py
@@ -14,34 +14,96 @@ from octoprint.util.version import get_octoprint_version_string
 from prometheus_client import make_wsgi_app
 from prometheus_client import Gauge
 from prometheus_client import Info
+from prometheus_client import Counter
 
 class PrometheusExporterPlugin(octoprint.plugin.BlueprintPlugin,
-							   octoprint.plugin.StartupPlugin):
+							   octoprint.plugin.StartupPlugin,
+							   octoprint.plugin.ProgressPlugin,
+							   octoprint.plugin.EventHandlerPlugin):
 
 	# TEMP
-	temps_actual = Gauge('temperaturesActual', 'Reported temperatures', ['identifier'])
-	temps_target = Gauge('temperaturesTarget', 'Targeted temperatures', ['identifier'])
+	temps_actual = Gauge('temperatures_actual', 'Reported temperatures', ['identifier'])
+	temps_target = Gauge('temperatures_target', 'Targeted temperatures', ['identifier'])
 
 	def get_temp_update(self, comm, parsed_temps):
 		for k, v in parsed_temps.items():
 			if isinstance(v, tuple) and len(v) == 2:
-				self.temps_actual.labels(k).set(v[0])
-				self.temps_target.labels(k).set(v[1])
-		return parsed_temps
+				if not v[0] is None:
+					self.temps_actual.labels(k).set(v[0])
+				if not v[1] is None:
+					self.temps_target.labels(k).set(v[1])
+		pass
 
 	# INFO
-	octoprint_info = Info('octoprintInfos', 'Octoprint host informations')
+	octoprint_info = Info('octoprint_infos', 'Octoprint host informations')
 
 	def on_after_startup(self):
 		import socket
 		import platform
 		import time
 		self.octoprint_info.info({
-			'oxtoprint_version': get_octoprint_version_string(),
+			'octoprint_version': get_octoprint_version_string(),
 			'host': socket.gethostname(),
 			'platform': platform.system(),
 			'app_start': str(int(time.time())),
 		})
+		pass
+
+	# CLIENT NUM
+	client_num = Gauge('client_num', 'The number of connected clients')
+
+	def clientnum_inc(self):
+		self.client_num.inc()
+	def clientnum_dec(self):
+		self.client_num.dec()
+
+	# PRINTER STATE
+	printer_state = Info('printer_state', 'Printer connection info')
+
+	def set_printer_info(self, payload):
+		self.printer_state.info(payload)
+
+	# PRINTING
+	started_print_counter = Counter('started_prints', 'Started print jobs')
+	failed_print_counter = Counter('failed_prints', 'Failed print jobs')
+	done_print_counter = Counter('done_prints', 'Done print jobs')
+	cancelled_print_counter = Counter('cancelled_prints', 'Cancelled print jobs')
+
+	# TIMELAPSE
+	timelapse_counter = Counter('captured_timelapses', 'Timelapse captured')
+
+	# PRINT PROGRESS
+	print_progress = Gauge('print_progress', 'Print progress', ['path'])
+
+	# SLICE PROGRESS
+	slice_progress = Gauge('slice_progress', 'Slice progress', ['path'])
+
+	##~~ EventHandlerPlugin mixin
+	def on_event(self, event, payload):
+		if event == 'ClientOpened':
+			self.clientnum_inc()
+		if event == 'ClientClosed':
+			self.clientnum_dec()
+		if event == 'PrinterStateChanged':
+			self.set_printer_info(payload)
+		if event == 'PrintStarted':
+			self.started_print_counter.inc()
+		if event == 'PrintFailed':
+			self.failed_print_counter.inc()
+		if event == 'PrintDone':
+			self.done_print_counter.inc()
+		if event == 'PrintCancelled':
+			self.cancelled_print_counter.inc()
+		if event == 'CaptureDone':
+			self.timelapse_counter.inc()
+		pass
+
+	##~~ ProgressPlugin mixin
+	def on_print_progress(self, storage, path, progress):
+		self.print_progress.labels(path).set(progress)
+		pass
+	def	on_slicing_progress(self, slicer, source_location, source_path, destination_location, destination_path, progress):
+		self.slice_progress.labels(source_path).set(progress)
 		pass
 
 	# ENDPOINT
