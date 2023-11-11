c4010d825dac360ab79b669fc35036392ebb5e35
Christian Lölkes <christian.loelkes@gmail.com>
2023-11-11 09:43:49 +0100

    use more explicit variable naming

--- octoprint_prometheus_exporter/gcodeparser.py
+++ octoprint_prometheus_exporter/gcodeparser.py
@@ -24,12 +24,11 @@ class Gcode_parser(object):
     def reset(self):
         self.extrusion_counter = 0
         self.x_travel = 0
-        self.x = None
+        self.x_pos = None
         self.y_travel = 0
-        self.y = None
+        self.y_pos = None
         self.z_travel = 0
-        self.z = None
-        self.e = None
+        self.z_pos = None
         self.speed = None
         self.print_fan_speed = None
 
@@ -37,50 +36,49 @@ class Gcode_parser(object):
         """ returns a tuple (x,y,z,e,speed) or None
         """
 
-        m = self.MOVE_RE.match(line)
+        parsed = self.MOVE_RE.match(line)
 
-        if m is None:
+        if parsed is None:
             return None
 
-        m = self.X_COORD_RE.match(line)
-        x = float(m.groups()[0]) if m else None
+        parsed = self.X_COORD_RE.match(line)
+        x_target = float(parsed.groups()[0]) if parsed else None
 
-        m = self.Y_COORD_RE.match(line)
-        y = float(m.groups()[0]) if m else None
+        parsed = self.Y_COORD_RE.match(line)
+        y_target = float(parsed.groups()[0]) if parsed else None
 
-        m = self.Z_COORD_RE.match(line)
-        z = float(m.groups()[0]) if m else None
+        parsed = self.Z_COORD_RE.match(line)
+        z_target = float(parsed.groups()[0]) if parsed else None
 
-        m = self.E_COORD_RE.match(line)
-        e = float(m.groups()[0]) if m else None
+        parsed = self.E_COORD_RE.match(line)
+        e_target = float(parsed.groups()[0]) if parsed else None
 
-        m = self.SPEED_VAL_RE.match(line)
-        speed = float(m.groups()[0]) if m else None
+        parsed = self.SPEED_VAL_RE.match(line)
+        speed = float(parsed.groups()[0]) if parsed else None
 
-        return x, y, z, e, speed
+        return x_target, y_target, z_target, e_target, speed
 
     def parse_fan_speed(self, line):
         if self.FAN_SET_RE.match(line):
-            m = self.FAN_SPEED_RE.match(line)
-            return float(m.groups()[0]) if m else 255.0
+            parsed = self.FAN_SPEED_RE.match(line)
+            return float(parsed.groups()[0]) if parsed else 255.0
         return 0.0 if self.FAN_OFF_RE.match(line) else None
 
     def process_line(self, line):
         movement = self.parse_move_args(line)
         if movement:
-            (x, y, z, e, speed) = movement
-            if e:
-                self.extrusion_counter += e
-                self.e = e
-            if x:
-                self.x_travel += abs(self.x - x) if self.x else 0
-                self.x = x
-            if y:
-                self.y_travel += abs(self.y - y) if self.y else 0
-                self.y = y
-            if z:
-                self.z_travel += abs(self.z - z) if self.z else 0
-                self.z = z
+            x_target, y_target, z_target, e_target, speed = movement
+            if e_target:
+                self.extrusion_counter += e_target
+            if x_target:
+                self.x_travel += abs(self.x_pos - x_target) if self.x_pos else 0
+                self.x_pos = x_target
+            if y_target:
+                self.y_travel += abs(self.y_pos - y_target) if self.y_pos else 0
+                self.y_pos = y_target
+            if z_target:
+                self.z_travel += abs(self.z_pos - z_target) if self.z_pos else 0
+                self.z_pos = z_target
             if speed:
                 self.speed = speed
             return "movement"
